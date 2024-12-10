5a325b8df038dc2f1a82d068ece368495a50c21c
Ravikumar <ravindra954@gmail.com>
2024-12-10 10:56:50 +0530

    terraform merge function

--- function/ec2.tf
+++ function/ec2.tf
@@ -1,36 +1,34 @@
 resource "aws_instance" "terraform" {
-    count = length(var.instance_names)
+   count = length(var.aws_instance_names)
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     tags=merge(
         var.comman_tags ,
         {
-            Name= var.instance_names[count.index]
+            Name= var.aws_instance_names[count.index]
         }
     )  
 }
 resource "aws_security_group" "allow_ssh_terraform" {
-    name = "allow_ssh"
-    description = "allow port number 22"
-
-   egress = {
-     from_port=0
-     to_port=0
-     protocol="-1"
-     cidr_blocks=["0.0.0.0/0"]
-     ipv6_cidr_blocks=["::/0"]
-   }
-   ingress = {
-    from_port=22
-    to_port=22
-    protocol="tcp"
-    cidr_blocks=["0.0.0.0/0"]
-    ipv6_cidr_blocks=["::/0"]
-   }
-  tags = merge(
-    var.comman_tags ,
-    {
-        Name="allow_ssh"
+    name = "allow_sshh"
+    description = "allow port number 22 for ssh access"
+    egress  {
+        from_port =0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+        ipv6_cidr_blocks=["::/0"]
     }
-  )
+    ingress  {
+        from_port=22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+        ipv6_cidr_blocks=["::/0"]
+    }
+    tags = merge(
+        var.comman_tags ,{
+            Name="allow_sshh"
+        }
+    )
 }
